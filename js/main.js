d1fdc155ebb991b19dbc9828c12e11a26c914a5f
enzo <enzovcnt@gmail.com>
2024-11-13 19:24:36 +0100

    text shadow move with the mouse

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100;
+
 
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: heights } = hero;
@@ -7,8 +9,16 @@ function shadow(e) {
 
     console.log(this, e.target);
     if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
 
     }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / width * walk) - (walk / 2));
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+
 }
 
-hero.addEventListener('mousemove', shadow)
+hero.addEventListener('mousemove', shadow);
