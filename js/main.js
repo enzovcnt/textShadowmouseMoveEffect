420578db0822527e01a3e5e57014db11d1ecc9ca
enzo <enzovcnt@gmail.com>
2024-11-13 19:08:08 +0100

    coords of the mouse when pass hover the h1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+function shadow(e) {
+    console.log(e);
+}
+
+hero.addEventListener('mousemove', shadow)
